90546d59bb637067c6509ab4b36ec3aa6e49aa4c
abdulalimus <abdul.alimus@gmail.com>
2025-03-20 21:33:48 -0700

    Add files via upload

---
+++ Class2MultipleFunctions.py
@@ -0,0 +1,15 @@
+class Class2MultipleFunctions ():
+     def Subfields():
+        lists = ["Machine Learning", "Neural Networks", "Vision", "Robotics", "Speech Processing", "Natural Language Processing"]
+        print("Subfields in AI are: ")
+        for domain in lists:
+            print (domain)
+    
+    def OddEven(num):
+        if (num%2 == 0):
+           Message = "is Even Number"
+        else:
+           Message = "is Odd Number"
+        return Message 
+            
+    
